a1cb26283beec90c0f077aa3bfc960ed0207a0e2
toshiko1045 <104748158+toshiko1045@users.noreply.github.com>
2023-05-31 10:00:41 +0900

    __init__.py の更新

--- ImageProcessing/__init__.py
+++ ImageProcessing/__init__.py
@@ -1,2 +1,2 @@
-from .main import func1  # 同じフォルダのmain.pyにあるfunc1をimport
+import .main
 __all__ = ['kernel_to_image', 'NMS_to_image', 'HT_to_image', 'threshold_otsu', 'threshold_adapt', 'erode_to_image'] 
