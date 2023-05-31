688fad620ff9755b9a8c5c3360f435bb357c9927
toshiko1045 <104748158+toshiko1045@users.noreply.github.com>
2023-05-31 10:01:23 +0900

    setup.py の更新

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ImageProcessing',  # パッケージ名（pip listで表示される）
-    version="0.0.1",  # バージョン
-    description="to process image",  # 説明
-    author='toshiko',  # 作者名
-    packages=find_packages(),  # 使うモジュール一覧を指定する
-    license='toshiko'  # ライセンス
+    name='ImageProcessing',  
+    version="0.0.1", 
+    description="to process image",  
+    author='toshiko',  
+    packages=find_packages(),  
+    license='toshiko'  
 )
